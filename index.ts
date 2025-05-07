9a1bb8131548101b25d63ca2d6882a739f327240
jubayerCodes <jubayerhossain111220@gmail.com>
2025-05-07 22:18:25 +0600

    solved 4 problems

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-console.log("Hello, World!");
+const formatString = (input: string, toUpper?: boolean): string => {
+  if (toUpper || typeof toUpper === "undefined") {
+    return input.toUpperCase();
+  } else {
+    return input.toLowerCase();
+  }
+};
+
+interface Book {
+  title: string;
+  rating: number;
+}
+
+const filterByRating = (books: Book[]): Book[] => {
+  const highestRatedBooks = books.filter((book) => book.rating >= 4);
+  return highestRatedBooks;
+};
+
+const concatenateArrays = <T>(...arrays: T[][]): T[] => {
+  const concatenatedArrays = arrays.flat();
+  return concatenatedArrays;
+};
+
+
